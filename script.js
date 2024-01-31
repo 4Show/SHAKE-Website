36d91171f274087c25f711494720b39d3775b842
4Show <4Showinvestments@gmail.com>
2024-01-30 18:30:50 -0600

    session storage uploading to checkout cart, needing to fix the cart count and cart items on onload. Also the css of the product items

--- script.js
+++ script.js
@@ -1,32 +1,36 @@
-
-// Display cart count on page load
-updateCartCountDisplay();
-
-function toggleCart(cartNode) 
+function toggleCart(cartNode, productId) 
 {
 
     const selectedButton = document.querySelector('.button-label input[type="radio"]:checked');
     if (selectedButton) 
     {
-        // Retrieve existing cart count from sessionStorage
-        const cartCount = parseInt(sessionStorage.getItem('cartCount')) ;
-
-        // Update the cart count
-        const newCartCount = cartCount + 1;
-        sessionStorage.setItem('cartCount', newCartCount);
-
-        // Update the cart count display
-        updateCartCountDisplay();
-        const cartList = document.getElementById('cartList');
-        const productName = selectedButton.dataset.name;
-        const productPrice = parseFloat(selectedButton.dataset.price);
-
-        const listItem = document.createElement('li');
-        listItem.textContent = `${productName} - $${productPrice.toFixed(2)}`;
-        cartList.appendChild(listItem);
-
-        // You can add more logic here, such as updating a total price.
+    
+         // Get existing cart data from sessionStorage
+         let cart = JSON.parse(sessionStorage.getItem('cart')) || [];
+         let cartCount = parseInt(sessionStorage.getItem('cartCount')) || 0;
         
+         let productContainer = cartNode.parentElement.parentElement
+
+
+        // Extract product information from the container
+        let product = {
+            name: productContainer.querySelector("div").querySelector("h3").innerHTML,
+            description: productContainer.querySelector("div").querySelector("p").innerHTML,
+            price: productContainer.querySelector("div").getElementsByClassName("price")[0].innerHTML,
+            // size: productContainer.querySelector("div").,
+            image: productContainer.querySelector("img").getAttribute("src")
+        };
+
+        //window.alert(productContainer.querySelector("div").getElementsByClassName("price")[0].innerHTML);
+
+        cart.push(product);
+        
+        cartCount++;
+
+         // Update sessionStorage with the new cart data
+        sessionStorage.setItem('cart', JSON.stringify(cart));
+        sessionStorage.setItem('cartCount', cartCount);
+        updateCartCountDisplay();
     }
     else 
     {
@@ -37,16 +41,73 @@ function toggleCart(cartNode)
 
 }
 
+function loadCart()
+{
+
+    // Get cart data from sessionStorage
+    let cart = JSON.parse(sessionStorage.getItem('cart'));
+    // window.alert(cart.length);
+
+
+    // Display cart content
+    const cartContent = document.getElementById('cartContent');
+
+
+    for(i =0; i < cart.length -1 ; i++)
+    {
+        if (cart.length == 0) 
+        {
+            const para = document.createElement("p");
+            const node = document.createTextNode("Your cart is empty.");
+            para.appendChild(node);
+
+            cartContent.appendChild(para);
+        } 
+        else
+        {
+            //create div element
+            let divContainer = document.createElement("div");
+            
+
+            //create the children of the div element
+
+            //gets the image
+            let img = document.createElement("img");
+            img.setAttribute('src',cart[i].image);
+            divContainer.appendChild(img);
+            
+
+            // gets the name           
+            let par2 = document.createElement("p");
+            par2.innerText = cart[i].name; 
+            divContainer.appendChild(par2);
+            
+            
+
+            // gets the price
+            let par = document.createElement("p");
+            par.innerText= cart[i].price; 
+            divContainer.appendChild(par);
+            
+
+            //append the information to the cartContainer
+            cartContent.appendChild(divContainer);
+            
+                            
+        }
+    }
+}
 
 function updateCartCountDisplay() {
+
+
     // Retrieve and display the cart count
-    const cartCount = parseInt(sessionStorage.getItem('cartCount')) ;
+    let cartCount = parseInt(sessionStorage.getItem('cartCount')) || 0;
     const cartCountElement = document.getElementById('cartCount');
     if (cartCountElement) {
       cartCountElement.textContent = cartCount;
     }
-  }
-
+}
 
 
 //this function will take the user to the checkout page after clicking the cart icon
@@ -77,21 +138,6 @@ function submitContactForm()
 
 //when the button is clicked within the specific product item
 //I want the background color of the button to change to red
-function highlightSize(selectedButton, productName) 
-{
-
-    //select the product item you are currently under ot identify the right information to send
-    var product = document.getElementById(productName);
-    //get the buttons under this element
-    var buttons = product.querySelectorAll("button");
-    
-    // Remove "highlighted" class from all buttons
-    buttons.forEach(btn => btn.classList.remove('.highlighted'));
-
-    // Add "highlighted" class to the selected button
-    buttons[selectedButton - 1].classList.add('.highlighted');
-}    
-
 //SELECTS THE correct size button for each product
 function highlightButton(label) 
 {
