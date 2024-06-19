5daab454584b89d323a0998c25e5691ebdbfbab8
4Show <4Showinvestments@gmail.com>
2024-06-19 04:07:19 -0500

    fixing square payment create  payment link

--- script.js
+++ script.js
@@ -447,91 +447,55 @@ function closeCart()
 async function callPaymentLink()
 {
 
-    // const awsEndpoint = 'https://wbib1en70a.execute-api.us-east-1.amazonaws.com/SquareAPI';
     const awsEndpoint = "https://82nxujrefe.execute-api.us-east-1.amazonaws.com/createPaymentLink";
     const functionURL = "https://b44ax3y3wncvazueob7tlu53iu0jjscm.lambda-url.us-east-1.on.aws/";
-    // var params = new URLSearchParams();
-    // var lineOrderItems = []
+    var params = new URLSearchParams();
+    var lineOrderItems = []
 
-    // // Get cart data from sessionStorage
-    // let cart = JSON.parse(sessionStorage.getItem('cart'))||[];
+    // Get cart data from sessionStorage
+    let cart = JSON.parse(sessionStorage.getItem('cart'))||[];
 
-    // for(let i = 0; i< cart.length; i++)
-    // {
-    //     var cartID = cart[i].cartId;
-    //     var quantity = cart[i].quantity;
+    for(let i = 0; i< cart.length; i++)
+    {
+        var cartID = cart[i].cartId;
+        var quantity = cart[i].quantity;
 
         
-    //     lineOrderItems[i] =  {
-    //         "quantity": quantity,
-    //         "catalog_object_id": cartID,
-    //         "item_type": "ITEM"
-    //         }
-    // }
+        lineOrderItems[i] =  {
+            "quantity": quantity,
+            "catalog_object_id": cartID,
+            "item_type": "ITEM"
+            }
+    }
     
-    // // window.alert(params);
-    // // Use the parameters in a URL
-    // // var urlWithParams = `${url}?${params.toString()}`;
-    // bodyContent = {
-    //     "description": "SHK Apparel",
-    //     "order": {
-    //     "location_id": "L96PW6T2031NW",
-    //     "line_items": [
-    //         {
-    //         "quantity": "1",
-    //         "catalog_object_id": "YE5IZG3MRWPGUTU6AAHJJOLR",
-    //         "item_type": "ITEM"
-    //         },
-    //         {
-    //         "quantity": "1",
-    //         "catalog_object_id": "XPW67JJWOQUJT63ANITAESSV",
-    //         "item_type": "ITEM"
-    //         },
-    //         {
-    //         "quantity": "1",
-    //         "catalog_object_id": "XPW67JJWOQUJT63ANITAESSV",
-    //         "item_type": "ITEM"
-    //         }
-    //     ]
-    //     } 
-    //    }
+    // window.alert(params);
+    // Use the parameters in a URL
+    // var urlWithParams = `${url}?${params.toString()}`;
+    bodyContent = {
+        "description": "SHK Apparel",
+        "order": {
+        "location_id": "L96PW6T2031NW",
+        "line_items": [
+            {
+            "quantity": "1",
+            "catalog_object_id": "YE5IZG3MRWPGUTU6AAHJJOLR",
+            "item_type": "ITEM"
+            },
+            {
+            "quantity": "1",
+            "catalog_object_id": "XPW67JJWOQUJT63ANITAESSV",
+            "item_type": "ITEM"
+            },
+            {
+            "quantity": "1",
+            "catalog_object_id": "XPW67JJWOQUJT63ANITAESSV",
+            "item_type": "ITEM"
+            }
+        ]
+        } 
+       }
 
-    // bodyContentFull = {
-    //     "description": "SHK Apparel",
-    //     "order": {
-    //     "location_id": "L96PW6T2031NW",
-    //     "line_items": [
-    //         {
-    //         "quantity": "1",
-    //         "catalog_object_id": "YE5IZG3MRWPGUTU6AAHJJOLR",
-    //         "item_type": "ITEM"
-    //         },
-    //         {
-    //         "quantity": "1",
-    //         "catalog_object_id": "XPW67JJWOQUJT63ANITAESSV",
-    //         "item_type": "ITEM"
-    //         },
-    //         {
-    //         "quantity": "1",
-    //         "catalog_object_id": "XPW67JJWOQUJT63ANITAESSV",
-    //         "item_type": "ITEM"
-    //         }
-    //     ]
-    //     },
-    //     "checkout_options": {
-    //     "allow_tipping": False,
-    //     "merchant_support_email": "4showinvestments@gmail.com",
-    //     "ask_for_shipping_address": True,
-    //     "accepted_payment_methods": {
-    //         "apple_pay": True,
-    //         "google_pay": True,
-    //         "cash_app_pay": True,
-    //         "afterpay_clearpay": True
-    //     },
-    //     "enable_coupon": False,
-    //     "enable_loyalty": False
-    //     }
-    // }
+   
 
     try {
         const response = await fetch(awsEndpoint, 
