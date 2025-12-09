bb67a77d4db1dd6c7bada63f8e6b96902793dd02
4Show <4Showinvestments@gmail.com>
2025-12-08 19:02:12 -0600

    deletion of theverisons foile

--- shake-aws-iac/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "your-terraform-state-bucket"
-    key            = "terraform/state"
-    region         = "us-west-2"
-    dynamodb_table = "terraform-locks"
-  }
-}
